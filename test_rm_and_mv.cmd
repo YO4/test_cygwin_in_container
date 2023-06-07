ab25eceef32710140ce40e80b1c4b6f8ab81fdfc
YO4 <ysno@ac.auone-net.jp>
2023-06-07 21:01:47 +0900

    cygdrive path

--- test_rm_and_mv.cmd
+++ test_rm_and_mv.cmd
@@ -6,19 +6,19 @@ set result=0
 echo;
 echo === normal directory : prepare
 
-sh -x -c "rm -rf /c/normal_dir && mkdir -p /c/normal_dir"
-sh -x -c "touch /c/normal_dir/file_to_unlink"
-sh -x -c "touch /c/normal_dir/file_to_rename"
-sh -x -c "find /c/normal_dir -type f"
+sh -x -c "rm -rf /cygdrive/c/normal_dir && mkdir -p /cygdrive/c/normal_dir"
+sh -x -c "touch /cygdrive/c/normal_dir/file_to_unlink"
+sh -x -c "touch /cygdrive/c/normal_dir/file_to_rename"
+sh -x -c "find /cygdrive/c/normal_dir -type f"
 
 echo;
 echo === normal directory : test
 
-sh -x -c "strace -o /c/opt/rm_on_normal_dir.trace rm /c/normal_dir/file_to_unlink"
+sh -x -c "strace -o /cygdrive/c/opt/rm_on_normal_dir.trace rm /cygdrive/c/normal_dir/file_to_unlink"
 call :a
-sh -x -c "strace -o /c/opt/mv_on_normal_dir.trace mv /c/normal_dir/file_to_rename /c/normal_dir/rename_complete"
+sh -x -c "strace -o /cygdrive/c/opt/mv_on_normal_dir.trace mv /cygdrive/c/normal_dir/file_to_rename /cygdrive/c/normal_dir/rename_complete"
 call :a
-sh -x -c "find /c/normal_dir -type f"
+sh -x -c "find /cygdrive/c/normal_dir -type f"
 
 echo;
 echo === bind mounted directory : prepare
@@ -26,18 +26,18 @@ echo === bind mounted directory : prepare
 @echo on
 del /q c:\binded_dir\*
 @echo off
-sh -x -c "touch /c/binded_dir/file_to_unlink"
-sh -x -c "touch /c/binded_dir/file_to_rename"
-sh -x -c "find /c/binded_dir -type f"
+sh -x -c "touch /cygdrive/c/binded_dir/file_to_unlink"
+sh -x -c "touch /cygdrive/c/binded_dir/file_to_rename"
+sh -x -c "find /cygdrive/c/binded_dir -type f"
 
 echo;
 echo === bind mounted directory : test
 
-sh -x -c "strace -o /c/opt/rm_on_mounted_dir.trace rm /c/binded_dir/file_to_unlink"
+sh -x -c "strace -o /cygdrive/c/opt/rm_on_mounted_dir.trace rm /cygdrive/c/binded_dir/file_to_unlink"
 call :a
-sh -x -c "strace -o /c/opt/mv_on_mounted_dir.trace mv /c/binded_dir/file_to_rename /c/binded_dir/rename_complete"
+sh -x -c "strace -o /cygdrive/c/opt/mv_on_mounted_dir.trace mv /cygdrive/c/binded_dir/file_to_rename /cygdrive/c/binded_dir/rename_complete"
 call :a
-sh -x -c "find /c/binded_dir -type f"
+sh -x -c "find /cygdrive/c/binded_dir -type f"
 
 echo;
 echo === crossing directory mv : prepare
@@ -45,19 +45,19 @@ echo === crossing directory mv : prepare
 @echo on
 del /q c:\binded_dir\*
 @echo off
-sh -x -c "rm -rf /c/normal_dir && mkdir -p /c/normal_dir"
-sh -x -c "touch /c/normal_dir/normal_to_bind"
-sh -x -c "touch /c/binded_dir/binded_to_normal"
-sh -x -c "(find /c/normal_dir /c/binded_dir -type f)"
+sh -x -c "rm -rf /cygdrive/c/normal_dir && mkdir -p /cygdrive/c/normal_dir"
+sh -x -c "touch /cygdrive/c/normal_dir/normal_to_bind"
+sh -x -c "touch /cygdrive/c/binded_dir/binded_to_normal"
+sh -x -c "(find /cygdrive/c/normal_dir /cygdrive/c/binded_dir -type f)"
 
 echo;
 echo === crossing directory mv : test
 
-sh -x -c "strace -o /c/opt/mv_normal_dir_to_mounted_dir.trace mv /c/normal_dir/normal_to_bind /c/binded_dir/to_binded_complete"
+sh -x -c "strace -o /cygdrive/c/opt/mv_normal_dir_to_mounted_dir.trace mv /cygdrive/c/normal_dir/normal_to_bind /cygdrive/c/binded_dir/to_binded_complete"
 call :a
-sh -x -c "strace -o /c/opt/mv_mounted_dir_to_normal_dir.trace mv /c/binded_dir/binded_to_normal /c/normal_dir/to_normal_complete"
+sh -x -c "strace -o /cygdrive/c/opt/mv_mounted_dir_to_normal_dir.trace mv /cygdrive/c/binded_dir/binded_to_normal /cygdrive/c/normal_dir/to_normal_complete"
 call :a
-sh -x -c "(find /c/normal_dir /c/binded_dir -type f)"
+sh -x -c "(find /cygdrive/c/normal_dir /cygdrive/c/binded_dir -type f)"
 
 echo;
 echo === container environment
